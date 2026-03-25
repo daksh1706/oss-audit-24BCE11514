e384db9eb912cc28a7dfbb3dcbc6ae521d244558
dakshmaru <dakshmaru10@gmail.com>
2026-03-25 22:11:55 +0530

    Update author information in manifesto generator script

--- script5_manifesto_generator.sh
+++ script5_manifesto_generator.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # ============================================================
 # Script 5: Open Source Manifesto Generator
-# Author: [Your Name] | Roll: [Your Roll Number]
+# Author: Daksh Maru | Roll: 24BCE11514
 # Course: Open Source Software | Software Choice: Git
 # Description: Asks the user 3 questions interactively and
 #              generates a personalised open source manifesto
