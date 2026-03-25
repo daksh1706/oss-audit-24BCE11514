d6a2f211aa6be686407ced501d6b2db42cb2f0f8
dakshmaru <dakshmaru10@gmail.com>
2026-03-25 22:10:30 +0530

    Update author information in the script header

--- script3_disk_permission_auditor.sh
+++ script3_disk_permission_auditor.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # ============================================================
 # Script 3: Disk and Permission Auditor
-# Author: [Your Name] | Roll: [Your Roll Number]
+# Author: Daksh Maru | Roll: 24BCE11514
 # Course: Open Source Software | Software Choice: Git
 # Description: Loops through key system directories and
 #              reports their size, owner, and permissions
